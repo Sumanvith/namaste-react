b70437dd15c173c75c95b4dac2db673935232e50
Sumanvith <dellbilla@gmail.com>
2024-05-11 22:14:27 +0530

    New Update

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ const parent = React.createElement(
     ),
     React.createElement(
         "div",
-        {id:"child2"},
+        {id:"child"},
         [React.createElement("h1",{},"I'm an h1 tag"),React.createElement("h2",{},"I'm an h2 tag")]
     )]
 );
