71277897259f14b0aa9edbad9e78b34bde7b6ec6
Sumanvith <dellbilla@gmail.com>
2024-05-24 23:09:10 +0530

    EP4 1/4

--- App.js
+++ App.js
@@ -1,22 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+/*
+Header
+  Logo
+  Nav Items
+Body
+  Search
+  RestaurantContainer
+    RestaurantCard
+Footer
+  Copyright
+  Links
+  Address
+  Contact
+*/
 
-const Title =()=> (
-  <h1 className="head" tabIndex="5">
-    Namaste React Using JSX
-  </h1>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&txt_keyword=All"
+        />
+      </div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const HeadingCompenent = () => (
-  <div id="container">
-    {Title()}
-    <Title/>
-    <Title></Title>
-    <h1 className="heading">Namaste React Functional Compenent</h1>
-  </div>
-);
+const AppLaayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingCompenent/>);
+root.render(<AppLaayout />);
