b0d16a91716ed2ae4dfb7817d6c16754adca31c7
Sumanvith <dellbilla@gmail.com>
2024-05-26 23:01:18 +0530

    EP6 1/3

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
+
 
 const Body = () => {
-  const [listOfRestaurants, setlistOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setlistOfRestaurants] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setlistOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+  if(listOfRestaurants.length===0){
+    return <h1>Loading....</h1>
+  }
   return (
     <div className="body">
       <div className="filter">
