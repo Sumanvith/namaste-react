84692146bca3feebd11b25ff304c0d86135667b8
Sumanvith <dellbilla@gmail.com>
2024-06-01 15:31:47 +0530

    EP7 3/5

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import { LOGO_URL } from "../utils/constants";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [btnNameReact, setbtnNameReact] = useState("Login");
-  useEffect(()=>{},[]);
   return (
     <div className="header">
       <div className="logo-container">
@@ -11,10 +11,18 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/Cart">Cart</Link>
+          </li>
           <button
             className="login"
             onClick={() => {
