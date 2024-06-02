950fc68fff2e2a1bef10bbc890154fc503ac8c34
Sumanvith <dellbilla@gmail.com>
2024-06-02 20:22:12 +0530

    EP8 5/5

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,28 @@
+import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is Namaste React</h2>
-      <UserClass
-        name={"Sumanvith (class)"}
-        location={"Hyderabad (class)"}></UserClass>
-    </div>
-  );
-};
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    //console.log("Parent Constructor");
+  }
+  componentDidMount() {
+    //console.log("Parent Mount");
+  }
+  render() {
+    //console.log("Parent Render");
+    return (
+      <div>
+        <h1>About Class Component</h1>
+        <h2>This is Namaste React</h2>
+        <UserClass
+          name={"Sumanvith (class)"}
+          location={"Hyderabad (class)"}></UserClass>
+      </div>
+    );
+  }
+}
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,21 +2,39 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { count: 0 };
+    this.state = {
+      userInfo: {
+        login: "Dummy",
+        location: "default",
+      },
+    };
+    //console.log(this.props.name + "Child Constructor");
   }
+  async componentDidMount() {
+    //console.log(this.props.name + "Child Mount");
+    const data = await fetch("https://api.github.com/users/Sumanvith");
+    const json = await data.json();
+    this.setState({
+      userInfo: json,
+    });
+    console.log(json);
+    this.timer = setInterval(() => {
+      console.log("componentDidMount");
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+    console.log("componentWillUnmount");
+  }
+
   render() {
-    const { name, location } = this.props;
-    const { count } = this.state;
+    const { login, location, avatar_url } = this.state.userInfo;
+    //console.log(this.props.name + "Child Render");
     return (
       <div className="user-card">
-        <h1>Count = {count}</h1>
-        <button
-          onClick={() => {
-            this.setState({ count: this.state.count + 1 });
-          }}>
-          Count Increases
-        </button>
-        <h2>Name: {name}</h2>
+        <img src={avatar_url}></img>
+        <h2>Name: {login}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: @sumanvith16</h4>
       </div>
