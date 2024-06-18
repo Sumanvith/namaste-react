6ec8d2866f547acce43ff8d42c8d332d9c474eb6
Sumanvith <dellbilla@gmail.com>
2024-06-18 12:04:19 +0530

    EP13 3/8

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -1,18 +1,26 @@
 import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
-test("should load contact", () => {
-  render(<Contact></Contact>);
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument();
-});
-test("should load contact", () => {
-  render(<Contact></Contact>);
-  const button = screen.getByText("Submit");
-  expect(button).toBeInTheDocument();
-});
-test("should load contact", () => {
-  render(<Contact></Contact>);
-  const inputName = screen.getByPlaceholderText("name");
-  expect(inputName).toBeInTheDocument();
+
+describe("Contact Page", () => {
+  test("should load contact", () => {
+    render(<Contact></Contact>);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
+  test("should load contact", () => {
+    render(<Contact></Contact>);
+    const button = screen.getByText("Submit");
+    expect(button).toBeInTheDocument();
+  });
+  it("should load contact", () => {
+    render(<Contact></Contact>);
+    const inputName = screen.getByPlaceholderText("name");
+    expect(inputName).toBeInTheDocument();
+  });
+  it("should load contact", () => {
+    render(<Contact></Contact>);
+    const inputBoxes = screen.getAllByRole("textbox");
+    expect(inputBoxes.length).not.toBe(3);
+  });
 });
